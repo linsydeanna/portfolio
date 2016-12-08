6075f7f369faf7057f674be25bebf9a958bbb286
Linsy Joyner <Linsy.Joyner@gmail.com>
2016-12-08 09:25:50 -0500

    skills edit

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 var express = require('express');
 
-var app = express()
-
-var autoprefixer = require('autoprefixer');
-var postcss      = require('postcss');
-// var css = require('./scss/main.scss');
-
-// postcss([ autoprefixer ]).process(css).then(function (result) {
-//     result.warnings().forEach(function (warn) {
-//         console.warn(warn.toString());
-//     });
-//     console.log(result.css);
-// });
+var app = express();
 
 app.use(express.static(__dirname));
 app.use('*', function (req, res) {
@@ -20,4 +9,4 @@ app.use('*', function (req, res) {
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
-})
+});
